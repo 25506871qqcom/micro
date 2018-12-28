a8550c297cf890cf084170814290a72ebc435b95
lyp <25506871@qq.com>
2018-12-28 22:34:07 +0800

    filter work token cookie

--- api-gateway/src/main/java/net/xdclass/apigateway/filter/LoginFilter.java
+++ api-gateway/src/main/java/net/xdclass/apigateway/filter/LoginFilter.java
@@ -3,14 +3,21 @@ package net.xdclass.apigateway.filter;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 
+import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 
 
 @Component
 public class LoginFilter extends ZuulFilter {
+
+    public static final String TOKEN = "token";
+
     @Override
     public String filterType() {
         return FilterConstants.PRE_TYPE;
@@ -28,18 +35,45 @@ public class LoginFilter extends ZuulFilter {
     @Override
     public boolean shouldFilter() {
 
-        RequestContext currentContext = RequestContext.getCurrentContext();
-        HttpServletRequest request = currentContext.getRequest();
+        RequestContext requestContext = getHttpServletRequest();
+        HttpServletRequest request = requestContext.getRequest();
 
-        System.out.println(request.getRemoteAddr());
-        System.out.println(request.getRequestURI());
-        System.out.println(request.getRequestURI());
+        String requestURI = request.getRequestURI();
+        System.out.println(requestURI);
+
+        if(StringUtils.contains(requestURI,"order/api/")){
+            System.out.println("not need filter");
+            return false;
+        }
+
+        System.out.println(" need filter");
         return true;
+    }
+
+    private RequestContext getHttpServletRequest() {
+        RequestContext currentContext = RequestContext.getCurrentContext();
+        return currentContext;
     }
 
     @Override
     public Object run() throws ZuulException {
         System.out.println("loginfilter  --- pre ");
+
+        RequestContext requestContext = getHttpServletRequest();
+        HttpServletRequest request = requestContext.getRequest();
+
+        String token = request.getHeader(TOKEN);
+
+        if(StringUtils.isEmpty(token)){
+            token = request.getParameter(TOKEN);
+        }
+
+        System.out.println("token="+token);
+
+        if(StringUtils.isEmpty(token)){
+            requestContext.setSendZuulResponse(false);
+            requestContext.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value() );
+        }
 
         return null;
     }
